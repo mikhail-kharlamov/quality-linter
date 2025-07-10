957a1e6a9b37ea8a9d564419f6f998f1cc895ae4
michaelharlam <palptin2014@gmail.com>
2025-07-10 03:08:26 +0300

    fix: List is changed for list in return type of function

--- app/func_for_test.py
+++ app/func_for_test.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
-def func(number1: int, number2: int) -> List[int]:
+def func(number1: int, number2: int) -> list[int]:
     return [number1 + number2, number1 * number2, number1 - number2,
             number1 // number2 if number2 != 0 else 0]
