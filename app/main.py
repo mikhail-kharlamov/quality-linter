4dabe42a948806bf1c0a573c2e4dffa1bd705964
Mikhail Kharlamov <120271217+mikhail-kharlamov@users.noreply.github.com>
2025-07-20 14:15:23 +0300

    feat: mvp for code review is ready (#2)
    * feat: mvp for code review is ready
    
    * feat: added simple validation
    
    * fix: style1
    
    * feat: added dataset with comparison of human- and llm-review
    
    * fix: style2
    
    ---------
    
    Co-authored-by: michaelharlam <palptin2014@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,39 +1,38 @@
+import json
 import os
 
-import openai
 from load_dotenv import load_dotenv
 
-
-class LlmApiClient:
-    def __init__(self, api_key: str, model_name: str = "gpt-3.5-turbo"):
-        self.api_key = api_key
-        self.client = openai.OpenAI(api_key=api_key)
-        self.model_name = model_name
-
-    def create_response(self, prompt: str) -> str:
-        try:
-            response = self.client.responses.create(
-                model=self.model_name,
-                input=[
-                    {
-                        "role": "user",
-                        "content": prompt
-                    }
-                ]
-            )
-            text = response.output_text
-        except Exception as e:
-            text = f"Ошибка при обращении к API OpenAI: {str(e)}"
-
-        return text
+from app.llm_api_client import LlmApiClient
+from app.parser import Parser
+from app.reviewer import Reviewer
+from dtos import CriteriaDto
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
     model_name = os.getenv("OPENAI_MODEL_NAME", "gpt-3.5-turbo")
 
-    client = LlmApiClient(api_key=api_key, model_name=model_name)
-    prompt = "Представь, что ты python-разработчик. Напиши алгоритм быстрой сортировки на python"
+    llm_api_client = LlmApiClient(api_key=api_key, model_name=model_name)
+    parser = Parser()
+    reviewer = Reviewer(llm_api_client=llm_api_client, parser=parser)
 
-    print(client.create_response(prompt))
+    paths_to_code = "/Users/mikhailkharlamov/Documents/Projects/SummerSchool/quality-linter/dataset/file_"
+    criteria = CriteriaDto(min_mark=0, max_mark=100)
+    for i in range(1, 11):
+        path = paths_to_code + str(i)
+
+        with open(path + "/auto-review.json", 'w', encoding="utf-8") as f:
+            review_result = reviewer.review(path + "/code.json", criteria).to_dict()
+            try:
+                json.dump(review_result, f, ensure_ascii=False, indent=4)
+                print("Review Result:", review_result)
+            except ValueError as e:
+                print("Review Error:", e)
+            except Exception as e:
+                print("An unexpected error occurred:", e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ dtos.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+from typing import Union
+
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class CommentDto:
+    path: str = field(default="")
+    start_line: int = field(default=0)
+    end_line: int = field(default=0)
+    original_start_line: int = field(default=0)
+    original_end_line: int = field(default=0)
+    body: str = field(default="")
+
+
+@dataclass_json
+@dataclass
+class FileDto:
+    name: str = field(default="")
+    extension: str = field(default="")
+    is_binary: bool = field(default=False)
+    path: str = field(default="")
+    content: str = field(default="")
+
+
+@dataclass_json
+@dataclass
+class DirectoryDto:
+    name: str = field(default="")
+    path: str = field(default="")
+    content: list[Union[FileDto, 'DirectoryDto']] = field(default_factory=list)
+
+
+@dataclass_json
+@dataclass
+class AutoReviewDto:
+    mark: int = field(default=0)
+    comments: list[CommentDto] = field(default_factory=list)
+
+
+@dataclass_json
+@dataclass
+class CriteriaDto:
+    min_mark: int = field(default=0)
+    max_mark: int = field(default=100)
